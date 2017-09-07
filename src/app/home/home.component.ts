e2beb81533af1d483d31be078cb9ee2c41be07cc
devin <the.devin.carter@outlook.com>
2017-09-07 15:47:10 -0500

    import Input decorator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = 'Welcome to the ninja directory!';
-  ninja = {
-    name: 'Yoshi',
-    belt: 'Black'
-  };
+  @Input() ninja;
 
   constructor() { }
 
